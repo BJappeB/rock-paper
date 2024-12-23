11302362702712f5e2a9d82be8ac73532e960be0
BjappeB <jasper.lahtonen@gmail.com>
2024-12-23 18:24:54 +0200

    Logic behind round has been made. Now all that is left is to make it whole game that last for 5 rounds

--- javascript.js
+++ javascript.js
@@ -2,25 +2,70 @@
 //Generate random number and attach the number to a maching string
 //if random number is 1 it is rock, if it is 2 then its paper otherwise scissors
 //Generate random number 
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
 const ranNum =  Math.floor(Math.random() * 3) + 1;
 if(ranNum === 1){
-    return console.log("Rock")
+    return "rock"
 }
 if(ranNum === 2){
-   return console.log("Paper")
+   return "paper"
 }
 else{
-   return console.log("Scissors")
+   return "scissors"
 }
 }
+//Get the users choice and store the choice the user made
+//prompt the choices
+//save the choice to a variable
 
 function getHumanChoice(){
+let choice = prompt("Pick paper, rock or scissors")
+return choice.toLowerCase();
+
+}
+//Get the humanChoice and ComputerChoice and then determine which one wins the round
+//Get the humanChoice 
+
+function playRound(humanChoice, computerChoice){
+humanChoice.toLowerCase()
+if(humanChoice == "rock" && computerChoice == "paper"){
+console.log("You lose!")
+computerScore++
+}
+if(humanChoice == "rock" && computerChoice == "scissors"){
+console.log("You win!")
+humanScore++
+}
+if(humanChoice == "paper" && computerChoice == "rock"){
+console.log("You win!")
+humanScore++
+}
+if(humanChoice == "paper" && computerChoice == "scissors"){
+console.log("You lose!")
+computerScore++
+}
+if(humanChoice == "scissors" && computerChoice == "rock"){
+console.log("You lose!")
+computerScore++
+}
+if(humanChoice == "scissors" && computerChoice == "paper"){
+console.log("You win!")
+humanScore++
+}
+if(humanChoice == computerChoice){
+   console.log("You tied")
+}
 
 }
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-
-
-console.log(getComputerChoice())
+playRound(humanSelection, computerSelection);
+console.log(humanSelection);
+console.log(computerSelection);
+console.log(humanScore)
+console.log(computerScore)
